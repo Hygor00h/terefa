6d16e6ef96154d8750c87e5c635d1e1b9bddd8d5
Hygor <hygorbr28@gmail.com>
2025-08-26 15:13:21 -0300

    git colocanto o bem

--- src/main/java/com/desafiogem/criador_tarefas/infrastructure/business/service/TarefaService.java
+++ src/main/java/com/desafiogem/criador_tarefas/infrastructure/business/service/TarefaService.java
@@ -1,4 +1,21 @@
 package com.desafiogem.criador_tarefas.infrastructure.business.service;
 
+import com.desafiogem.criador_tarefas.infrastructure.entity.Tarefa;
+import com.desafiogem.criador_tarefas.infrastructure.repository.TarefasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TarefaService {
+
+    private final TarefasRepository tarefasRepository;
+
+    @Autowired
+    public TarefaService(TarefasRepository tarefasRepository) {
+        this.tarefasRepository = tarefasRepository;
+    }
+
+    public Tarefa salvaTarefa(Tarefa tarefa){
+        return tarefasRepository.save(tarefa);
+    }
 }
--- src/main/java/com/desafiogem/criador_tarefas/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/desafiogem/criador_tarefas/infrastructure/repository/TarefasRepository.java
@@ -2,6 +2,8 @@ package com.desafiogem.criador_tarefas.infrastructure.repository;
 
 import com.desafiogem.criador_tarefas.infrastructure.entity.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarefasRepository extends JpaRepository <Tarefa, Long> {
 }
